ab7c8b76f372466f6ff25b28f116f5e09dc168b0
Mateuszao <mateusgarcia@id.uff.br>
2020-09-16 21:39:03 -0300

    Segundo commit

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function countdown() {
     var now = new Date()
-    var eventDate = new Date(2020, 09, 21, 16)
+    var eventDate = new Date(2020, 08, 21, 16)
 
     var currentTime = now.getTime()
     var eventTime = eventDate.getTime()
